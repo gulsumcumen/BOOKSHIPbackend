864d092c998afdd3e367c062e0056afb151af443
gulsumcumen <gulsumcumen@gmail.com>
2024-08-26 10:34:55 +0300

    Add project files.

---
+++ BOOKSHIPBACKEND/Controllers/BookshipController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BOOKSHIP.Controllers
+{
+    public class BookshipController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        public IActionResult Forum()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
